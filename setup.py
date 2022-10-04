61031286bb07e7818d7848d5587546ee066cf398
Sohan Dhanak <Sohan.Dhanak36@thehutgroup.com>
2022-10-04 09:47:47 +0100

    Added templates to package data

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name = 'netbox-static-routes',
-    version = '0.1', # would be nice to import the version from netbox_static_routes/version.py
+    version = '0.1.1', # would be nice to import the version from netbox_static_routes/version.py
     description = 'Manage static routes in Netbox',
     url = 'https://github.com/jbparrish17/netbox-static-routes',
     author = 'Joshua Parrish',
@@ -10,5 +10,8 @@ setup(
     install_requires = [],
     packages = find_packages(),
     include_package_data = True,
+    package_data = {
+        'netbox_static_routes': ['templates/*/*.html']
+    },
     zip_safe = False
 )
